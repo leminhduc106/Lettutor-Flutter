7418be4e625d2a84ac689877652845840a10d414
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-03-06 13:33:47 +0700

    Feat: add custom button and textfield

---
+++ lettutor_flutter/lib/widgets/custom_button/custom_button.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import '../../utils/base_style.dart';
+
+class CustomButton {
+  static Widget common(
+      {required Function() onTap,
+      required String content,
+      bool? isLoading,
+      bool isShadow = true}) {
+    Widget text = Text(
+      content,
+      style: BaseTextStyle.button(color: Colors.white),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+    Widget loading = const SizedBox(
+      height: 24,
+      width: 24,
+      child: CircularProgressIndicator(strokeWidth: 2.5, color: Colors.white),
+    );
+    Widget button = Container(
+        height: 48,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        decoration: BoxDecoration(
+            color: BaseColor.blue,
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [if (isShadow) BaseBoxShadow.componentBoxShadow]),
+        child: (isLoading == true) ? loading : text);
+    return GestureDetector(onTap: onTap, child: button);
+  }
+
+  static Widget small({required VoidCallback onTap, required String content}) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+                color: BaseColor.blue,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0xff003399).withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 8,
+                      offset: const Offset(0, 4))
+                ]),
+            child: Text(content,
+                style: BaseTextStyle.subtitle2(color: Colors.white))));
+  }
+
+  static Widget withIcon(
+      {required VoidCallback onTap,
+      required String content,
+      required String iconPath}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        decoration: BoxDecoration(
+            color: const Color.fromRGBO(0, 51, 153, 1),
+            borderRadius: BorderRadius.circular(16.0)),
+        child: Row(
+          children: [
+            Image.asset(
+              iconPath,
+              width: 20,
+              height: 20,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 6),
+            Text(content, style: BaseTextStyle.subtitle1(color: Colors.white)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  static Widget whiteBackGround(
+      {required VoidCallback onTap,
+      required String content,
+      bool isShadow = true,
+      Color textColor = BaseColor.blue,
+      Color outlineColor = BaseColor.blue}) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.0),
+                border: Border.all(color: outlineColor, width: 0.5),
+                boxShadow: [
+                  if (isShadow)
+                    BoxShadow(
+                        color: const Color(0xff003399).withOpacity(0.2),
+                        spreadRadius: 0,
+                        blurRadius: 8,
+                        offset: const Offset(0, 2))
+                ]),
+            child:
+                Text(content, style: BaseTextStyle.button(color: textColor))));
+  }
+
+  static Widget lightBlue(
+      {required Function() onTap,
+      required String content,
+      bool? isLoading,
+      bool isShadow = true}) {
+    Widget text = Text(
+      content,
+      style: BaseTextStyle.button(color: BaseColor.blue),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+    Widget loading = const SizedBox(
+      height: 24,
+      width: 24,
+      child: CircularProgressIndicator(strokeWidth: 2.5, color: Colors.white),
+    );
+    Widget button = Container(
+        height: 48,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        decoration: BoxDecoration(
+            color: BaseColor.secondaryBlue,
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [if (isShadow) BaseBoxShadow.componentBoxShadow]),
+        child: (isLoading == true) ? loading : text);
+    return GestureDetector(onTap: onTap, child: button);
+  }
+
+  static Widget whiteBtnWithIcon(
+      {required VoidCallback onTap,
+      required String content,
+      bool isShadow = true,
+      Color textColor = BaseColor.blue,
+      Color outlineColor = BaseColor.blue}) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.0),
+                border: Border.all(color: outlineColor, width: 0.5),
+                boxShadow: [
+                  if (isShadow)
+                    BoxShadow(
+                        color: const Color(0xff003399).withOpacity(0.2),
+                        spreadRadius: 0,
+                        blurRadius: 8,
+                        offset: const Offset(0, 2))
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 24,
+                  child: Image.asset(
+                    "assets/icons/action/icon_add_square.png",
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Text(content, style: BaseTextStyle.button(color: textColor)),
+              ],
+            )));
+  }
+
+  static Widget transparentWithTrailingIcon(
+      {required VoidCallback onTap,
+      required String content,
+      bool isShadow = true,
+      Color textColor = BaseColor.blue,
+      Color outlineColor = BaseColor.blue}) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(color: outlineColor, width: 0.5),
+                boxShadow: [
+                  if (isShadow)
+                    BoxShadow(
+                        color: const Color(0xff003399).withOpacity(0.2),
+                        spreadRadius: 0,
+                        blurRadius: 8,
+                        offset: const Offset(0, 2))
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(content, style: BaseTextStyle.button(color: textColor)),
+                SizedBox(
+                  height: 16,
+                  child: Image.asset(
+                    "assets/icons/directions/blue_next.png",
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ],
+            )));
+  }
+}
---
+++ lettutor_flutter/lib/widgets/custom_textfield/custom_textfield.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:pronest/utils/base_style.dart';
+
+class CustomTextField {
+  static Widget common(
+      {required ValueChanged<String> onChanged,
+      String? labelText,
+      String? hintText,
+      String? errorText,
+      String? initialValue,
+      bool required = false,
+      bool isObscured = false,
+      bool enable = true,
+      EdgeInsets? contentPadding,
+      FocusNode? focusNode,
+      TextEditingController? textEditingController,
+      TextCapitalization textCapitalization = TextCapitalization.none,
+      bool autoFocus = false,
+      int? maxLength,
+      int? maxLines,
+      Color? fillColor,
+      bool readOnly = false,
+      TextStyle? textStyle,
+      TextAlign textAlign = TextAlign.start,
+      TextAlignVertical textAlignVertical = TextAlignVertical.center,
+      TextInputAction? textInputAction,
+      VoidCallback? edit,
+      VoidCallback? onPrefixIconTap,
+      String? prefixIconPath,
+      String? editIconPath,
+      TextInputType? textInputType}) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if ((labelText != null))
+            Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Row(mainAxisSize: MainAxisSize.min, children: [
+                  Text(labelText, style: BaseTextStyle.subtitle2()),
+                  if (required == true)
+                    Text(" *",
+                        style: BaseTextStyle.subtitle2(color: BaseColor.red))
+                ])),
+          Container(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  boxShadow: [BaseBoxShadow.common]),
+              child: TextFormField(
+                  initialValue: initialValue,
+                  onChanged: onChanged,
+                  enabled: enable,
+                  readOnly: readOnly,
+                  obscureText: isObscured,
+                  obscuringCharacter: "*",
+                  style: textStyle ?? BaseTextStyle.body2(),
+                  focusNode: focusNode,
+                  controller: textEditingController,
+                  autofocus: autoFocus,
+                  textAlign: textAlign,
+                  textCapitalization: textCapitalization,
+                  textAlignVertical: textAlignVertical,
+                  textInputAction: textInputAction,
+                  maxLength: maxLength,
+                  maxLines: maxLines ?? 1,
+                  keyboardType: textInputType,
+                  decoration: InputDecoration(
+                      prefixIcon: onPrefixIconTap != null
+                          ? IconButton(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              onPressed: () {
+                                onPrefixIconTap();
+                              },
+                              icon: ClipRRect(
+                                child: Image.asset(
+                                  prefixIconPath ??
+                                      "assets/icons/action/icon_search.png",
+                                  height: 24,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ))
+                          : null,
+                      suffixIcon: edit != null
+                          ? IconButton(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              onPressed: () {
+                                edit();
+                              },
+                              icon: ClipRRect(
+                                child: Image.asset(
+                                  editIconPath ??
+                                      "assets/icons/action/icon_edit.png",
+                                  height: 20,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                            )
+                          : null,
+                      hintText: hintText,
+                      counter: const Offstage(),
+                      fillColor: fillColor ??
+                          (readOnly ? BaseColor.lightGrey : Colors.white),
+                      filled: true,
+                      contentPadding: contentPadding ??
+                          const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                      hintStyle: BaseTextStyle.body2(color: BaseColor.hint),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          borderSide: BorderSide(
+                            color: errorText != null
+                                ? BaseColor.red
+                                : Colors.transparent,
+                            width: 0.0,
+                          )),
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                            width: 0.0,
+                          )),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          borderSide: BorderSide(
+                              color: errorText != null
+                                  ? BaseColor.red
+                                  : readOnly
+                                      ? Colors.white
+                                      : BaseColor.blue,
+                              width: 1.0)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          borderSide: const BorderSide(
+                              color: BaseColor.red, width: 1.0)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          borderSide: const BorderSide(
+                              color: BaseColor.red, width: 1.0))))),
+          if (errorText != null)
+            Text(errorText, style: BaseTextStyle.caption(color: BaseColor.red))
+        ]);
+  }
+}
