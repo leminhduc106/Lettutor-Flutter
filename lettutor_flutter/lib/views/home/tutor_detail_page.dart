beadea4104873f3658485ed16571452ef0afe76e
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-03-10 10:38:34 +0700

    Feat: add Tutor information in tutor_detail page

--- lettutor_flutter/lib/views/home/tutor_detail_page.dart
+++ lettutor_flutter/lib/views/home/tutor_detail_page.dart
@@ -3,10 +3,8 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:lettutor_flutter/utils/base_style.dart';
 import 'package:lettutor_flutter/widgets/custom_appbar/custom_appbar.dart';
-import 'package:lettutor_flutter/widgets/custom_button/custom_button.dart';
 import 'package:lettutor_flutter/widgets/video_player/video_player.dart';
-import 'package:video_player/video_player.dart';
-import 'package:chewie/chewie.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class TutorDetailPage extends StatefulWidget {
   const TutorDetailPage({Key? key}) : super(key: key);
@@ -19,6 +17,12 @@ class _TutorDetailPageState extends State<TutorDetailPage> {
   double _rating = 88;
   bool _isExpanded = false;
   int maxLength = 100;
+  List<String> _tutorItems = [
+    'Tiếng Anh cho công việc',
+    'Giao tiếp',
+    'IELTS',
+    'TOEIC'
+  ];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -45,6 +49,7 @@ class _TutorDetailPageState extends State<TutorDetailPage> {
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
           child: Column(
             mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -106,11 +111,13 @@ class _TutorDetailPageState extends State<TutorDetailPage> {
               _isExpanded
                   ? Text(
                       text,
-                      style: BaseTextStyle.body2(fontSize: 14),
+                      style: BaseTextStyle.body2(
+                          fontSize: 14, color: Colors.black54),
                     )
                   : Text(
                       text,
-                      style: BaseTextStyle.body2(fontSize: 14),
+                      style: BaseTextStyle.body2(
+                          fontSize: 14, color: Colors.black54),
                       overflow: TextOverflow.ellipsis,
                       maxLines: 3,
                     ),
@@ -151,18 +158,126 @@ class _TutorDetailPageState extends State<TutorDetailPage> {
                 ],
               ),
               Container(
-                padding: const EdgeInsets.all(12),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
                 color: Colors.transparent,
                 width: safeWidth,
                 height: heightSafeArea * 0.3,
                 child: VideoPlayerPage(),
               ),
+              const SizedBox(height: 18),
+              _buildTutorInfor(_tutorItems),
+              const SizedBox(height: 18),
+              // Expanded(
+              //   child: SfCalendar(
+              //     view: CalendarView.week,
+
+              //   ),
+              // ),
             ],
           ),
         ),
       )),
     );
   }
+}
+
+Widget _buildTutorInfor(List<String> tutorItems) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        "Ngôn ngữ",
+        style: BaseTextStyle.heading2(fontSize: 18),
+      ),
+      Container(
+        margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 208, 227, 247),
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+        child: Text("English",
+            style: BaseTextStyle.body2(fontSize: 14, color: Colors.blue)),
+      ),
+      Text(
+        "Chuyên ngành",
+        style: BaseTextStyle.heading2(fontSize: 18),
+      ),
+      const SizedBox(height: 16),
+      Wrap(
+        spacing: 10.0,
+        runSpacing: 14.0,
+        children: tutorItems.map<Widget>((item) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 208, 227, 247),
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+              child: Text(item,
+                  style: BaseTextStyle.body2(fontSize: 14, color: Colors.blue)),
+            ),
+          );
+        }).toList(),
+      ),
+      const SizedBox(height: 24),
+      Text(
+        "Khóa học tham khảo",
+        style: BaseTextStyle.heading2(fontSize: 18),
+      ),
+      const SizedBox(height: 8),
+      Row(children: [
+        Text(
+          "Basic Conversation Topics:",
+          style: BaseTextStyle.heading4(fontSize: 16),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            "Tìm hiểu",
+            style: BaseTextStyle.heading2(fontSize: 14, color: Colors.blue),
+          ),
+        ),
+      ]),
+      Row(children: [
+        Text(
+          "Life in the Internet Age:",
+          style: BaseTextStyle.heading4(fontSize: 16),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            "Tìm hiểu",
+            style: BaseTextStyle.heading2(fontSize: 14, color: Colors.blue),
+          ),
+        ),
+      ]),
+      const SizedBox(height: 24),
+      Text(
+        "Sở thích",
+        style: BaseTextStyle.heading2(fontSize: 18),
+      ),
+      const SizedBox(height: 8),
+      Text(
+        "I loved the weather, the scenery and the laid-back lifestyle of the locals.",
+        style: BaseTextStyle.body2(fontSize: 14, color: Colors.black54),
+      ),
+      const SizedBox(height: 24),
+      Text(
+        "Kinh nghiệm giảng dạy",
+        style: BaseTextStyle.heading2(fontSize: 18),
+      ),
+      const SizedBox(height: 8),
+      Text(
+        "I have more than 10 years of teaching English experience.",
+        style: BaseTextStyle.body2(fontSize: 14, color: Colors.black54),
+      ),
+    ],
+  );
 }
 
 Widget _buildTutorUtilities(
