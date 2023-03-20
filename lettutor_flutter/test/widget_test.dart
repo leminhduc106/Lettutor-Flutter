89434310a785a10bb97d4c6fddb43fa804342acf
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-03-20 23:20:36 +0700

    Feat: add menu items for bottomNavigationBar

--- lettutor_flutter/test/widget_test.dart
+++ lettutor_flutter/test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lettutor_flutter/app.dart';
 
 import 'package:lettutor_flutter/main.dart';
 
