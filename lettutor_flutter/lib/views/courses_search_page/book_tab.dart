190498af649ec19cf6c2f956383a8858858598e6
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-04-22 10:36:47 +0700

    Refactor: refactor book tab with api call

--- lettutor_flutter/lib/views/courses_search_page/book_tab.dart
+++ lettutor_flutter/lib/views/courses_search_page/book_tab.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lettutor_flutter/data/book_sample.dart';
-import 'package:lettutor_flutter/models/book/book.dart';
-import 'package:woozy_search/woozy_search.dart';
+import 'package:lettutor_flutter/global_state/app_provider.dart';
+import 'package:lettutor_flutter/global_state/auth_provider.dart';
+import 'package:lettutor_flutter/models/course_model/course_category.dart';
+import 'package:lettutor_flutter/models/ebook_model/ebook_model.dart';
 import 'package:lettutor_flutter/routes/routes.dart' as routes;
+import 'package:lettutor_flutter/services/ebook_service.dart';
+import 'package:provider/provider.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BookTab extends StatefulWidget {
   const BookTab({Key? key}) : super(key: key);
@@ -15,28 +22,138 @@ class BookTab extends StatefulWidget {
 }
 
 class _BookTabState extends State<BookTab> {
-  List<Book> _results = [];
+  List<Ebook> _results = [];
   final TextEditingController _controller = TextEditingController();
-
-  // * Debounce timer for search performance
+  final listLevels = {
+    "0": "Any level",
+    "1": "Beginner",
+    "2": "High Beginner",
+    "3": "Pre-Intermediate",
+    "4": "Intermediate",
+    "5": "Upper-Intermediate",
+    "6": "Advanced",
+    "7": "Proficiency"
+  };
   Timer? _debounce;
+  String category = "";
+  String search = "";
+  bool isLoading = true;
+  bool isLoadMore = false;
+  int page = 1;
+  int perPage = 10;
+  late ScrollController _scrollController;
+  String? token;
+
+  List<Widget> _generateChips(List<CourseCategory> categories) {
+    return categories
+        .map(
+          (chip) => GestureDetector(
+            onTap: () {
+              if (category == chip.id) {
+                setState(() {
+                  category = "";
+                  _results = [];
+                  page = 1;
+                  isLoading = true;
+                });
+              } else {
+                setState(() {
+                  category = chip.id;
+                  _results = [];
+                  page = 1;
+                  isLoading = true;
+                });
+              }
+            },
+            child: Container(
+              margin: const EdgeInsets.only(top: 5, right: 8),
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              decoration: BoxDecoration(
+                color: chip.id == category ? Colors.blue[50] : Colors.grey[200],
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                border: Border.all(
+                    color: chip.id == category
+                        ? Colors.blue[100] as Color
+                        : Colors.grey[400] as Color),
+              ),
+              child: Text(
+                chip.title,
+                style: TextStyle(
+                  fontSize: 12,
+                  color:
+                      chip.id == category ? Colors.blue[400] : Colors.grey[600],
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(loadMore);
+  }
 
   @override
   void dispose() {
     _debounce?.cancel();
+    _scrollController.removeListener(loadMore);
     super.dispose();
+  }
+
+  void fetchListEbook(int page, int size, String token) async {
+    final response = await EbookService.getListEbookWithPagination(
+        page, size, token,
+        categoryId: category, q: search);
+    if (mounted) {
+      setState(() {
+        _results.addAll(response);
+        isLoading = false;
+      });
+    }
+  }
+
+  void loadMore() async {
+    if (_scrollController.position.extentAfter < page * perPage) {
+      setState(() {
+        isLoadMore = true;
+        page++;
+      });
+
+      try {
+        List<Ebook> response = await EbookService.getListEbookWithPagination(
+            page, perPage, token as String,
+            categoryId: category, q: search);
+        if (mounted) {
+          setState(() {
+            _results.addAll(response);
+            isLoadMore = false;
+          });
+        }
+      } catch (e) {
+        showTopSnackBar(
+            context, const CustomSnackBar.error(message: "Cannot load more"),
+            showOutAnimationDuration: const Duration(milliseconds: 1000),
+            displayDuration: const Duration(microseconds: 4000));
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final woozy = Woozy();
-    List<String> names = BooksSample.books.map((book) => book.name).toList();
-    woozy.setEntries(names);
+    final authProvider = Provider.of<AuthProvider>(context);
+    final appProvider = Provider.of<AppProvider>(context);
+    final lang = appProvider.language;
 
-    if (_controller.text.isEmpty) {
-      setState(() {
-        _results = BooksSample.books;
-      });
+    setState(() {
+      token = authProvider.tokens!.access.token;
+    });
+
+    if (isLoading) {
+      fetchListEbook(1, 10, authProvider.tokens!.access.token);
     }
 
     return Column(
@@ -49,17 +166,11 @@ class _BookTabState extends State<BookTab> {
             onChanged: (value) {
               if (_debounce?.isActive ?? false) _debounce?.cancel();
               _debounce = Timer(const Duration(milliseconds: 500), () {
-                final res = woozy.search(value);
-                List<Book> newResults = [];
-                for (int i = 0; i < res.length; i++) {
-                  if (res[i].score >= 0.3) {
-                    newResults.add(BooksSample.books
-                        .firstWhere((book) => book.name == res[i].text));
-                  }
-                }
-
                 setState(() {
-                  _results = newResults;
+                  search = value;
+                  _results = [];
+                  page = 1;
+                  isLoading = true;
                 });
               });
             },
@@ -80,101 +191,128 @@ class _BookTabState extends State<BookTab> {
                     Radius.circular(10),
                   ),
                 ),
-                hintText: "Search books..."),
+                hintText: lang.searchEbook),
           ),
         ),
-        Expanded(
-          child: _controller.text.isNotEmpty && _results.isEmpty
-              ? SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          "assets/svg/ic_notfound.svg",
-                          width: 200,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 20),
-                          child: Text(
-                            "Not found any match result...",
-                            style: TextStyle(color: Colors.grey[700]),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+          height: 37,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: _generateChips(appProvider.allCourseCategories).length,
+            itemBuilder: (context, index) {
+              return _generateChips(appProvider.allCourseCategories)[index];
+            },
+            shrinkWrap: true,
+          ),
+        ),
+        isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Expanded(
+                child: _results.isEmpty
+                    ? SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                "assets/svg/ic_notfound.svg",
+                                width: 200,
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 20),
+                                child: Text(
+                                  lang.errNotAnyResult,
+                                  style: TextStyle(color: Colors.grey[700]),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                )
-              : ListView.builder(
-                  itemCount: _results.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.pushNamed(context, routes.bookDetailPage);
-                        },
-                        child: Card(
-                          elevation: 8,
-                          child: SizedBox(
-                            height: 305,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset(
-                                  _results[index].img,
-                                  alignment: Alignment.center,
-                                  width: MediaQuery.of(context).size.width,
-                                  height: 170,
-                                  fit: BoxFit.cover,
-                                ),
-                                Container(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(10, 10, 10, 15),
+                      )
+                    : ListView.builder(
+                        itemCount: _results.length,
+                        controller: _scrollController,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: GestureDetector(
+                              onTap: () async {
+                                if (await canLaunch(_results[index].fileUrl)) {
+                                  await launch(_results[index].fileUrl);
+                                }
+                              },
+                              child: Card(
+                                elevation: 8,
+                                child: SizedBox(
+                                  height: 600,
                                   child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
-                                      Text(
-                                        _results[index].name,
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold),
+                                      CachedNetworkImage(
+                                        imageUrl: _results[index].imageUrl,
+                                        fit: BoxFit.cover,
+                                        progressIndicatorBuilder: (context, url,
+                                                downloadProgress) =>
+                                            CircularProgressIndicator(
+                                                value:
+                                                    downloadProgress.progress),
+                                        errorWidget: (context, url, error) =>
+                                            const Icon(Icons.error),
                                       ),
                                       Container(
-                                        margin: const EdgeInsets.only(
-                                            top: 8, bottom: 15),
-                                        child: Text(
-                                          _results[index].description,
-                                          maxLines: 2,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.grey[800]),
+                                        padding: const EdgeInsets.fromLTRB(
+                                            10, 10, 10, 15),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              _results[index].name,
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                  fontSize: 17,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Container(
+                                              margin: const EdgeInsets.only(
+                                                  top: 8, bottom: 15),
+                                              child: Text(
+                                                _results[index].description,
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Colors.grey[800]),
+                                              ),
+                                            ),
+                                            Text(
+                                              listLevels[
+                                                      _results[index].level] ??
+                                                  "Any level",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.grey[800]),
+                                            ),
+                                          ],
                                         ),
-                                      ),
-                                      Text(
-                                        _results[index].level,
-                                        style: TextStyle(
-                                            fontSize: 15,
-                                            color: Colors.grey[800]),
-                                      ),
+                                      )
                                     ],
                                   ),
-                                )
-                              ],
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-        ),
+              ),
       ],
     );
   }
