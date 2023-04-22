ed57a3bcbe6ba7b407f809985aca610d2407a9e7
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-04-22 10:16:09 +0700

    Refactor: refactor about course component

--- lettutor_flutter/lib/views/course/components/about.dart
+++ lettutor_flutter/lib/views/course/components/about.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:lettutor_flutter/models/course/course.dart';
+import 'package:lettutor_flutter/global_state/app_provider.dart';
+import 'package:lettutor_flutter/models/course_model/course_model.dart';
 import 'package:lettutor_flutter/utils/base_style.dart';
+import 'package:provider/provider.dart';
 
 class AboutCourse extends StatelessWidget {
-  const AboutCourse({Key? key, required this.course}) : super(key: key);
+  AboutCourse({Key? key, required this.course}) : super(key: key);
 
   final Course course;
+  final listLevels = {
+    "0": "Any level",
+    "1": "Beginner",
+    "2": "High Beginner",
+    "3": "Pre-Intermediate",
+    "4": "Intermediate",
+    "5": "Upper-Intermediate",
+    "6": "Advanced",
+    "7": "Proficiency"
+  };
 
   @override
   Widget build(BuildContext context) {
+    final lang = Provider.of<AppProvider>(context).language;
+
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
@@ -19,14 +33,14 @@ class AboutCourse extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 8),
             child: Text(
-              "About Course",
+              lang.aboutCourse,
               style: BaseTextStyle.heading4(
                 fontSize: 30,
               ),
             ),
           ),
           Text(
-            course.about,
+            course.description,
             style: BaseTextStyle.body1(
               fontSize: 17,
               color: Colors.black54,
@@ -35,62 +49,85 @@ class AboutCourse extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(bottom: 0, top: 14),
             child: Text(
-              "Overview",
+              lang.overview,
               style: BaseTextStyle.heading4(
                 fontSize: 30,
               ),
             ),
           ),
-          ListView.builder(
-            itemCount: course.overview.length,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              String key = course.overview.keys.elementAt(index);
-              return Column(
+          Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.help_outline,
-                        color: Colors.red,
+                  const Icon(
+                    Icons.help_outline,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 70,
+                    margin:
+                        const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                    child: Text(
+                      lang.why,
+                      style: BaseTextStyle.heading2(
+                        fontSize: 17,
                       ),
-                      Container(
-                        width: MediaQuery.of(context).size.width - 70,
-                        margin: const EdgeInsets.only(
-                            left: 10, bottom: 10, top: 10),
-                        child: Text(
-                          key,
-                          style: BaseTextStyle.heading2(
-                            fontSize: 17,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Text(
-                    course.overview[key] as String,
-                    style: BaseTextStyle.body1(
-                      fontSize: 17,
-                      color: Colors.black54,
                     ),
-                  ),
+                  )
                 ],
-              );
-            },
+              ),
+              Text(
+                course.reason,
+                style: BaseTextStyle.body1(
+                  fontSize: 17,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.help_outline,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 70,
+                    margin:
+                        const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                    child: Text(
+                      lang.what,
+                      style: BaseTextStyle.heading2(
+                        fontSize: 17,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                course.purpose,
+                style: BaseTextStyle.body1(
+                  fontSize: 17,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 8.0, top: 14),
             child: Text(
-              "Level",
+              lang.level,
               style: BaseTextStyle.heading4(
                 fontSize: 30,
               ),
             ),
           ),
           Text(
-            course.level,
+            listLevels[course.level] ?? "Any level",
             style: BaseTextStyle.body1(
               fontSize: 17,
               color: Colors.black54,
