c74fcba7bdf2488a40927296cf2d07dea7f92df3
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-05-01 11:14:51 +0700

    Refactor: refactor search tutor with api call

--- lettutor_flutter/lib/views/tutors_search_page/tutor_item.dart
+++ lettutor_flutter/lib/views/tutors_search_page/tutor_item.dart
@@ -1,17 +1,49 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lettutor_flutter/models/tutor/tutor.dart';
+import 'package:lettutor_flutter/constants/learning_topics.dart';
+import 'package:lettutor_flutter/global_state/auth_provider.dart';
+import 'package:lettutor_flutter/models/tutor_model/tutor_info_model.dart';
+import 'package:lettutor_flutter/models/tutor_model/tutor_model.dart';
 import 'package:lettutor_flutter/routes/routes.dart' as routes;
+import 'package:lettutor_flutter/services/tutor_service.dart';
 import 'package:lettutor_flutter/utils/base_style.dart';
-import 'package:lettutor_flutter/widgets/avatar_circle/avatar_circle.dart';
+import 'package:provider/provider.dart';
 
-class TutorCardInfo extends StatelessWidget {
+class TutorCardInfo extends StatefulWidget {
   const TutorCardInfo({Key? key, required this.tutor}) : super(key: key);
 
-  final Tutor tutor;
+  final TutorInfo tutor;
+
+  @override
+  State<TutorCardInfo> createState() => _TutorCardInfoState();
+}
+
+class _TutorCardInfoState extends State<TutorCardInfo> {
+  Tutor? tutorDetail;
+  bool isLoading = true;
+
+  fetchTutorDetail(String token) async {
+    final tutorDetail = await TutorService.getTutor(widget.tutor.userId, token);
+    setState(() {
+      this.tutorDetail = tutorDetail;
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final specialist = listLearningTopics.entries
+        .where((element) =>
+            widget.tutor.specialties.split(",").contains(element.key))
+        .map((e) => e.value)
+        .toList();
+
+    final authProvider = Provider.of<AuthProvider>(context);
+    if (isLoading) {
+      fetchTutorDetail(authProvider.tokens!.access.token);
+    }
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: GestureDetector(
@@ -19,7 +51,7 @@ class TutorCardInfo extends StatelessWidget {
           Navigator.pushNamed(
             context,
             routes.tutorProfilePage,
-            arguments: {"tutor": tutor},
+            arguments: {"tutorID": widget.tutor.userId},
           );
         },
         child: Card(
@@ -37,10 +69,27 @@ class TutorCardInfo extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 15),
-                      child: AvatarCircle(
-                          width: 65, height: 65, source: tutor.image),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10, right: 10),
+                      height: 60,
+                      width: 60,
+                      child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(1000),
+                            child: CachedNetworkImage(
+                              imageUrl: widget.tutor.avatar as String,
+                              fit: BoxFit.cover,
+                              width: 70,
+                              height: 70,
+                              progressIndicatorBuilder:
+                                  (context, url, downloadProgress) =>
+                                      CircularProgressIndicator(
+                                          value: downloadProgress.progress),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          )),
                     ),
                     Expanded(
                       child: Column(
@@ -53,7 +102,7 @@ class TutorCardInfo extends StatelessWidget {
                                 child: Container(
                                   margin: const EdgeInsets.only(bottom: 5),
                                   child: Text(
-                                    tutor.fullName,
+                                    widget.tutor.name,
                                     style: BaseTextStyle.heading2(fontSize: 16),
                                     maxLines: 1,
                                     overflow: TextOverflow.ellipsis,
@@ -63,7 +112,9 @@ class TutorCardInfo extends StatelessWidget {
                               Row(
                                 children: [
                                   Text(
-                                    tutor.getTotalStar().toString(),
+                                    tutorDetail?.avgRating != null
+                                        ? tutorDetail!.avgRating.toString()
+                                        : "0",
                                     style: const TextStyle(
                                       fontSize: 17,
                                       fontWeight: FontWeight.w500,
@@ -71,37 +122,20 @@ class TutorCardInfo extends StatelessWidget {
                                     ),
                                   ),
                                   const SizedBox(width: 5),
-                                  SvgPicture.asset(
-                                    "assets/svg/ic_star.svg",
-                                    color: Colors.yellow,
-                                    height: 25,
-                                    width: 25,
-                                  ),
+                                  const Icon(Icons.star, color: Colors.yellow),
                                 ],
                               ),
                             ],
                           ),
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 28,
-                                  child: Image.asset(
-                                    "assets/icons/social/icon_vietnam_flag.png",
-                                    fit: BoxFit.fitHeight,
-                                  ),
-                                ),
-                                const SizedBox(width: 8),
-                                Text(
-                                  tutor.country,
-                                  style: BaseTextStyle.body2(),
-                                ),
-                              ]),
+                          Text(
+                            widget.tutor.country!,
+                            style: BaseTextStyle.body2(),
+                          ),
                           const SizedBox(height: 8),
                           SizedBox(
                             height: 35,
                             child: ListView.builder(
-                              itemCount: tutor.languages.length,
+                              itemCount: specialist.length,
                               scrollDirection: Axis.horizontal,
                               shrinkWrap: true,
                               itemBuilder: (context, index) {
@@ -115,7 +149,7 @@ class TutorCardInfo extends StatelessWidget {
                                         255, 212, 212, 212),
                                     borderRadius: BorderRadius.circular(32.0),
                                   ),
-                                  child: Text(tutor.languages[index],
+                                  child: Text(specialist[index],
                                       style: BaseTextStyle.body2(fontSize: 14)),
                                 );
                               },
@@ -130,7 +164,7 @@ class TutorCardInfo extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(
                       horizontal: 24.0, vertical: 16.0),
                   child: Text(
-                    tutor.intro,
+                    widget.tutor.bio,
                     style: BaseTextStyle.body2(fontSize: 15),
                     overflow: TextOverflow.ellipsis,
                     maxLines: 5,
--- lettutor_flutter/lib/views/tutors_search_page/tutors.dart
+++ lettutor_flutter/lib/views/tutors_search_page/tutors.dart
@@ -2,11 +2,15 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lettutor_flutter/data/tutors_sample.dart';
-import 'package:lettutor_flutter/models/tutor/tutor.dart';
-import 'package:lettutor_flutter/utils/base_style.dart';
+import 'package:lettutor_flutter/global_state/app_provider.dart';
+import 'package:lettutor_flutter/global_state/auth_provider.dart';
+import 'package:lettutor_flutter/models/tutor_model/tutor_info_model.dart';
+import 'package:lettutor_flutter/models/user_model/learning_topic_model.dart';
+import 'package:lettutor_flutter/services/tutor_service.dart';
 import 'package:lettutor_flutter/views/tutors_search_page/tutor_item.dart';
-import 'package:woozy_search/woozy_search.dart';
+import 'package:provider/provider.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class TutorsPage extends StatefulWidget {
   const TutorsPage({Key? key}) : super(key: key);
@@ -16,55 +20,53 @@ class TutorsPage extends StatefulWidget {
 }
 
 class _TutorsPageState extends State<TutorsPage> {
-  final List<String> _chips = [
-    "All",
-    "English for kids",
-    "English for Business",
-    "Conversational",
-    "STARTERS",
-    "MOVERS",
-    "FLYERS",
-    "KET",
-    "PET",
-    "IELTS",
-    "TOEFL",
-    "TOEIC"
-  ];
   final TextEditingController _controller = TextEditingController();
 
-  List<Tutor> _results = [];
-  String specialist = "All";
-
-  // * Debounce timer for search performance
+  List<TutorInfo> _results = [];
+  String specialist = "";
   Timer? _debounce;
+  String search = "";
+  bool isLoading = true;
+  bool isLoadMore = false;
+  int page = 1;
+  int perPage = 10;
+  late ScrollController _scrollController;
+  String? token;
 
-  List<Widget> _generateChips() {
-    return _chips
+  List<Widget> _generateChips(List<dynamic> chips) {
+    return chips
         .map(
           (chip) => GestureDetector(
             onTap: () {
               setState(() {
-                specialist = chip;
+                specialist = chip.key;
+                isLoading = true;
+                _results = [];
+                page = 1;
               });
             },
             child: Container(
               margin: const EdgeInsets.only(top: 5, right: 8),
-              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 4),
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
               decoration: BoxDecoration(
-                color: chip == specialist ? Colors.blue[50] : Colors.grey[200],
+                color:
+                    chip.key == specialist ? Colors.blue[50] : Colors.grey[200],
                 borderRadius: const BorderRadius.all(Radius.circular(20)),
                 border: Border.all(
-                    color: chip == specialist
+                    color: chip.key == specialist
                         ? Colors.blue[100] as Color
                         : Colors.grey[400] as Color),
               ),
-              child: Text(chip,
-                  style: BaseTextStyle.heading4(
-                    fontSize: 12,
-                    color: chip == specialist
-                        ? Colors.blue[400]
-                        : Colors.grey[600],
-                  )),
+              child: Text(
+                chip.name,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: chip.key == specialist
+                      ? Colors.blue[400]
+                      : Colors.grey[600],
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             ),
           ),
         )
@@ -72,124 +74,169 @@ class _TutorsPageState extends State<TutorsPage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(loadMore);
+  }
+
+  @override
   void dispose() {
     _debounce?.cancel();
+    _scrollController.removeListener(loadMore);
     super.dispose();
+  }
+
+  void fetchTutors(int page, int size, String token) async {
+    final response = await TutorService.searchTutor(page, size, token,
+        search: search, specialties: [specialist]);
+    if (mounted) {
+      setState(() {
+        _results.addAll(response);
+        isLoading = false;
+      });
+    }
+  }
+
+  void loadMore() async {
+    if (_scrollController.position.extentAfter < page * perPage) {
+      setState(() {
+        isLoadMore = true;
+        page++;
+      });
+
+      try {
+        final response = await TutorService.searchTutor(
+            page, perPage, token as String,
+            search: search, specialties: [specialist]);
+        if (mounted) {
+          setState(() {
+            _results.addAll(response);
+            isLoadMore = false;
+          });
+        }
+      } catch (e) {
+        showTopSnackBar(
+            context, const CustomSnackBar.error(message: "Cannot load more"),
+            showOutAnimationDuration: const Duration(milliseconds: 1000),
+            displayDuration: const Duration(microseconds: 4000));
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final woozy = Woozy();
-    List<String> names =
-        TutorsSample.tutors.map((tutor) => tutor.fullName).toList();
-    woozy.setEntries(names);
+    final authProvider = Provider.of<AuthProvider>(context);
+    final appProvider = Provider.of<AppProvider>(context);
+    final lang = appProvider.language;
 
-    if (_controller.text.isEmpty) {
-      setState(() {
-        _results = TutorsSample.tutors;
-      });
+    setState(() {
+      token = authProvider.tokens!.access.token;
+    });
+
+    List<dynamic> chips = [];
+    chips.add(LearnTopic(id: 1, key: "", name: "All"));
+    chips.addAll(appProvider.allLearningTopics);
+    chips.addAll(appProvider.allTestPreparations);
+
+    if (isLoading) {
+      fetchTutors(1, 10, token as String);
     }
 
-    if (specialist != "All") {
-      _results = _results
-          .where((tutor) => tutor.specialties.contains(specialist))
-          .toList();
-    }
-
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: TextField(
-                  style: TextStyle(fontSize: 12, color: Colors.grey[900]),
-                  controller: _controller,
-                  onChanged: (value) {
-                    if (_debounce?.isActive ?? false) _debounce?.cancel();
-                    _debounce = Timer(const Duration(milliseconds: 1000), () {
-                      final res = woozy.search(value);
-                      List<Tutor> newResults = [];
-                      for (int i = 0; i < res.length; i++) {
-                        if (res[i].score >= 0.3) {
-                          newResults.add(TutorsSample.tutors.firstWhere(
-                              (tutor) => tutor.fullName == res[i].text));
-                        }
-                      }
-
-                      setState(() {
-                        _results = newResults;
-                        if (specialist != "All") {
-                          _results = _results
-                              .where((tutor) =>
-                                  tutor.specialties.contains(specialist))
-                              .toList();
-                        }
-                      });
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: TextField(
+                style: TextStyle(fontSize: 12, color: Colors.grey[900]),
+                controller: _controller,
+                onChanged: (value) {
+                  if (_debounce?.isActive ?? false) _debounce?.cancel();
+                  _debounce = Timer(const Duration(milliseconds: 1000), () {
+                    setState(() {
+                      search = value;
+                      _results = [];
+                      page = 1;
+                      isLoading = true;
                     });
-                  },
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey.shade200,
-                      prefixIcon: Container(
-                        padding: const EdgeInsets.all(13),
-                        child: SvgPicture.asset(
-                          "assets/svg/ic_search.svg",
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                      contentPadding: const EdgeInsets.only(left: 5, right: 5),
-                      border: const OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      hintText: "Search Tutors")),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 12),
-              height: 35,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: _generateChips().length,
-                itemBuilder: (context, index) {
-                  return _generateChips()[index];
+                  });
                 },
-                shrinkWrap: true,
-              ),
-            ),
-            _controller.text.isNotEmpty && _results.isEmpty
-                ? SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset(
-                            "assets/svg/ic_notfound.svg",
-                            width: 200,
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 20),
-                            child: Text(
-                              "Not found any match result...",
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                          ),
-                        ],
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                    prefixIcon: Container(
+                      padding: const EdgeInsets.all(13),
+                      child: SvgPicture.asset(
+                        "assets/svg/ic_search.svg",
+                        color: Colors.grey[600],
                       ),
                     ),
-                  )
-                : ListView.builder(
-                    itemCount: _results.length,
-                    itemBuilder: (context, index) {
-                      Tutor tutor = _results[index];
-                      return TutorCardInfo(tutor: tutor);
-                    },
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                  ),
-          ],
-        ),
+                    contentPadding: const EdgeInsets.only(left: 5, right: 5),
+                    border: const OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    hintText: lang.searchTutor)),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 12),
+            height: 35,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: _generateChips(chips).length,
+              itemBuilder: (context, index) {
+                return _generateChips(chips)[index];
+              },
+              shrinkWrap: true,
+            ),
+          ),
+          isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Expanded(
+                  child: _results.isEmpty
+                      ? SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(
+                                  "assets/svg/ic_notfound.svg",
+                                  width: 200,
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 20),
+                                  child: Text(
+                                    lang.errNotAnyResult,
+                                    style: TextStyle(color: Colors.grey[700]),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: _results.length,
+                          controller: _scrollController,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: TutorCardInfo(tutor: _results[index]),
+                            );
+                          },
+                        ),
+                ),
+          if (isLoadMore)
+            const SizedBox(
+              height: 50,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
