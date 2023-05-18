be2debb1db9f75e6c6d71ad7044db3151bd2c9de
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-05-18 11:52:26 +0700

    Feat: add login with google feature

--- lettutor_flutter/lib/main.dart
+++ lettutor_flutter/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lettutor_flutter/app.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(
     const MyApp(),
   );
--- lettutor_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lettutor_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import firebase_auth
+import firebase_core
 import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
@@ -16,6 +18,8 @@ import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
